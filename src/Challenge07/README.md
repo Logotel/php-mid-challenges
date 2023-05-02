87fbeb0eed8d33b0c518f31f3d657ecec3ccb6b8
giagara <giagara@yahoo.it>
2023-05-02 10:31:39 +0200

    created challenge 07

---
+++ src/Challenge07/README.md
@@ -0,0 +1,18 @@
+# LOGOTEL CHALLANGE 07: MAGIC PHP 2 🪄
+
+### Goal
+
+Create the appropriate structure to handle test success.
+
+### What we will be focused on
+
+- Code style
+- Corde readability
+- Comments (if necessary)
+- Optimization
+- SOLID principles
+- The test should pass
+
+### Test
+
+Test with instruction in root directory.
---
+++ tests/Challenge07/Challenge07Test.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Logotel\ChallengeMidDev\Tests\Challenge07;
+
+use Logotel\ChallengeMidDev\Challenge07\ChallengeFacade;
+use Logotel\ChallengeMidDev\Challenge07\MediaPlayer;
+use PHPUnit\Framework\TestCase;
+
+class Challenge07Test extends TestCase
+{
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test_class_will_handle_correctly(string $name, array $arguments, int|string|null $expected)
+    {
+        
+        $result = ChallengeFacade::$name($arguments);
+
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public static function dataProvider()
+    {
+        return [
+            "correct" => [
+                "name" => "sum",
+                "arguments" => [100, 200],
+                "expected" => 300
+            ],
+            "not found" => [
+                "types" => "aclassthatnotexists",
+                "arguments" => ["test", "class"],
+                "expected" => null
+            ],
+            "correct 2" => [
+                "types" => "concat",
+                "arguments" => ["test", "class"],
+                "expected" => "this is a test class"
+            ],
+            "correct 3" => [
+                "types" => "sort",
+                "arguments" => [[9, 5, 4 ,7, 1, 3, 2, 6, 8]],
+                "expected" => [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            ],
+        ];
+    }
+    
+}
